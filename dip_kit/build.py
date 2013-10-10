c4579a48ce8cc7a7c0289c0e9df7a35e7d578fff
Ron DuPlain <ron@getprivatepractice.com>
2013-10-10 15:57:16 -0400

    Make builder aware of application provider.

--- dip_kit/build.py
+++ dip_kit/build.py
@@ -2,15 +2,25 @@
 
 import os
 
+from .provider import ApplicationProvider
+
 
 class Builder(object):
     # TODO: Add hooks for add_url_rule, before_/after_request, errorhandler.
 
-    def __init__(self, config=None):
+    def __init__(self, config=None, app_provider_class=None):
+        if app_provider_class is None:
+            self.app_provider_class = ApplicationProvider
+        else:
+            self.app_provider_class = app_provider_class
+
         self.config = {}
         if config is not None:
             self.config.update(config)
         self.routes = {}
+
+    def build_application_provider(self):
+        return self.app_provider_class(builder=self)
 
     def route(self, rule, methods=None, method=None):
         if method is not None:
